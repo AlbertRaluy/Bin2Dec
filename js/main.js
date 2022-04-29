27f927f32c1d9ea49711eb7a3e73a57faf318b26
Albert <albertraluy04@gmail.com>
2022-04-29 16:34:00 +0200

    final commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function parrafos() {
+    var Num1 = document.getElementById('Numº')
+    infonum.innerHTML = 'You entered a non binary digit (please enter only 0 or 1)'
+    var Num2 = document.getElementById('Num2')
+    if (numero << 1) {
+        var Num2 = 'Here is your decimal'
+    }
+}
